1d75a6fcb665d4df8982804ab6acac7129add284
227095 <227095@KB37-120-C03>
2025-02-18 08:44:23 +0300

    Добавьте файлы проекта.

---
+++ pr3_ch2_322_Salchuk_Matukhina/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace pr3_ch2_322_Salchuk_Matukhina
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
